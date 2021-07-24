d01ddf0633ade344e81e8d8dfbff11133e85151b
chenyongping <chenyongping001@gmail.com>
2021-07-24 13:31:16 +0800

    store add colleciton,Cart and OrderItem,CartItem

--- store/models.py
+++ store/models.py
@@ -1,6 +1,15 @@
+from typing_extensions import NotRequired
 from django.db import models
+from django.db.models.base import Model
+from django.db.models.deletion import CASCADE
+from django.db.models.fields import CharField
+from django.db.models.fields.related import ForeignKey
 
 # Create your models here.
+
+
+class Collection(models.Model):
+    title = CharField(max_length=255)
 
 
 class Product(models.Model):
@@ -9,3 +18,50 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
+    collection = ForeignKey(Collection, on_delete=models.PROTECT)
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255)
+    birth_date = models.DateField(null=True)
+
+
+class Order(models.Model):
+    PAYMENT_STATUS_PENDING = 'P'
+    PAYMENT_STATUS_COMPLETE = 'C'
+    PAYMENT_STATUS_FAILED = 'F'
+    PAYMENT_STATUS_CHOICES = [
+        (PAYMENT_STATUS_PENDING, 'Pending'),
+        (PAYMENT_STATUS_COMPLETE, 'Complete'),
+        (PAYMENT_STATUS_FAILED, 'Failed'),
+    ]
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = CharField(
+        max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
+    customer = ForeignKey(Customer, on_delete=models.PROTECT)
+
+
+class Address(models.Model):
+    street = CharField(max_length=255)
+    city = CharField(max_length=255)
+    customer = ForeignKey(Customer, on_delete=models.CASCADE)
+
+
+class OrderItem(models.Model):
+    order = ForeignKey(Order, on_delect=models.PROTECT)
+    product = models.ForeignKey(Product, on_delect=models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+
+class Cart(models.Model):
+    create_at = models.DateTimeField(auto_now_add=Ture)
+
+
+class CartItem(models.Model):
+    cart = ForeignKey(Cart, on_delete=models.CASCADE)
+    product = ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
