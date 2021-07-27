73a7c68b1d5a5f0febd29248089c9d81049fbe4b
chenyongping <chenyongping001@gmail.com>
2021-07-27 13:27:53 +0800

    test order_by,filter,sort,values,and so on

--- playgroud/views.py
+++ playgroud/views.py
@@ -1,5 +1,5 @@
 from typing import Collection
-from django.db.models import query
+from django.db.models import Q, F
 from django.shortcuts import render
 from django.http import HttpResponse
 from store.models import Customer, Order, OrderItem, Product, Collection
@@ -12,5 +12,30 @@ def say_hello(request):
     # queryset = Customer.objects.filter(email__contains='.com')
     # queryset = Collection.objects.filter(feature_product__isnull=True)
     # queryset = Product.objects.filter(inventory__lt=10)
-    queryset = OrderItem.objects.filter(product__collection__id=3)
+    # queryset = OrderItem.objects.filter(product__collection__id=3)
+
+    # queryset = Product.objects.filter(inventory__lt=10, unit_price__lt=20)
+    # queryset = Product.objects.filter(
+    # inventory__lt=10).filter(unit_price__lt=20)
+
+    # queryset = Product.objects.filter(
+    #     Q(inventory__lt=10) & ~Q(unit_price__lt=20))
+
+    # queryset = Product.objects.filter(inventory=F('collection__id'))
+
+    # queryset = Product.objects.order_by('unit_price', '-title').reverse()
+
+    # product = Product.objects.order_by('unit_price')[0]
+    # product = Product.objects.earliest('unit_price')
+    # product = Product.objects.latest('unit_price')
+
+    # queryset = Product.objects.filter(unit_price__lt=20)
+
+    # queryset = Product.objects.all()[5:10]
+
+    # queryset = Product.objects.values('id', 'title', 'collection__title')
+    # queryset = Product.objects.values_list('id', 'title', 'collection__title')
+    queryset = Product.objects.filter(
+        id__in=OrderItem.objects.values('product__id').distinct())
+
     return render(request, "hello.html", {"name": "mosh", "products": list(queryset)})
