52ef2a65b9f2ea81c908eae450b099d93364a480
kelemi <chenyongping001@gmail.com>
2022-11-04 21:32:38 +0800

    Django ORM-20221104

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,9 @@
-from typing import List
+
 from django.shortcuts import render
 from django.http import HttpResponse
-from store.models import Product
+from store.models import Product, OrderItem
 from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q, F
 
 
 def calculate():
@@ -13,13 +14,10 @@ def calculate():
 
 def say_hello(request):
     # x = calculate()
-    try:
-        product = Product.objects.get(pk=0)
-    except ObjectDoesNotExist:
-        pass
-    product = Product.objects.filter(pk=0).first()
-    exists = Product.objects.filter(pk=0).exists()
 
+    queryset = queryset = Product.objects.defer('description')
 
-
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    return render(request, 'hello.html', {
+        'name': 'kelemi',
+        'products': list(queryset)
+    })
